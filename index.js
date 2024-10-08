18b9f935f72a9dba06c4844f1f754bc509fa4132
44vin@44 <vintageclias@gmail.com>
2024-10-08 09:17:47 +0300

    array-lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+// Destructive functions
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  // Non-destructive functions
+  function appendCat(name) {
+    return [...cats, name]; // Return a new array with the new cat appended
+  }
+  
+  function prependCat(name) {
+    return [name, ...cats]; // Return a new array with the new cat prepended
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, -1); // Return a new array without the last cat
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1); // Return a new array without the first cat
+  }
+  
